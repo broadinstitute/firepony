0d409335ec8b62e767296b245de25e20556abe96
Nuno Subtil <nsubtil@nvidia.com>
2014-11-06 15:56:10 -0800

    build: Add option to use the system installed version of Boost
    Now that none of the downstream projects rely on the Boost build
    system, switch to using the system-wide installed version of Boost by
    default.

--- contrib/gamgee.cmake
+++ contrib/gamgee.cmake
@@ -1,14 +1,23 @@
 set(gamgee_PREFIX ${CMAKE_BINARY_DIR}/contrib/gamgee-prefix)
 set(gamgee_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/contrib/gamgee-install)
 
+if (USE_SYSTEM_BOOST)
+    set(BOOST_DEPENDS "")
+    set(BOOST_CMAKE_ARGS "")
+else()
+    set(BOOST_DEPENDS "boost")
+    set(BOOST_CMAKE_ARGS
+            -DBOOST_ROOT=${Boost_INSTALL_PREFIX}
+            -DBoost_NO_SYSTEM_PATHS=ON)
+endif()
+
 ExternalProject_Add(gamgee
     PREFIX ${gamgee_PREFIX}
-    DEPENDS boost
+    DEPENDS ${BOOST_DEPENDS}
     GIT_REPOSITORY "https://github.com/broadinstitute/gamgee.git"
     GIT_TAG "3631525493724f7a05f82be389167dda27c9eedc"
     INSTALL_DIR ${gamgee_PREFIX}/install
-    CMAKE_ARGS -DBOOST_ROOT=${Boost_INSTALL_PREFIX}
-               -DBoost_NO_SYSTEM_PATHS=ON
+    CMAKE_ARGS ${BOOST_CMAKE_ARGS}
                -DINSTALL_DEPENDENCIES=ON
                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_INSTALL_PREFIX=${gamgee_INSTALL_PREFIX}

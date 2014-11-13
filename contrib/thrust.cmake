a86658974e1021f5e5d838631bc065e507aae975
Nuno Subtil <nsubtil@nvidia.com>
2014-11-12 16:59:56 -0800

    build: Switch to building with external version of Thrust
    This brings in fixes to cross-space execution warnings that were
    showing up with the toolkit version of Thrust.

--- contrib/thrust.cmake
+++ contrib/thrust.cmake
@@ -3,7 +3,8 @@ set(thrust_PREFIX ${CMAKE_BINARY_DIR}/contrib/thrust-prefix)
 ExternalProject_Add(thrust
     PREFIX ${thrust_PREFIX}
     GIT_REPOSITORY "https://github.com/thrust/thrust.git"
-    GIT_TAG "1.8.0"
+    # we're tracking branch 1.8.0
+    GIT_TAG "c2863d38d107a225e9dae9bb4ff9a5d39b71ab3b"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""

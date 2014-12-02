4f9b09fdbf7d4f715f4044986b7446d081e71972
Nuno Subtil <nsubtil@nvidia.com>
2014-12-01 17:25:45 -0800

    build: Fix Boost external project build
    Make sure we can build with no system-wide Boost installation
    available. Removes most of the optional Boost libraries and adds a
    post-install script to fix up the Boost install directory.
    
    Also bumps the CUB version.

--- contrib/cub.cmake
+++ contrib/cub.cmake
@@ -2,7 +2,7 @@ set(cub_PREFIX ${CMAKE_BINARY_DIR}/contrib/cub-prefix)
 ExternalProject_Add(cub
     PREFIX ${cub_PREFIX}
     GIT_REPOSITORY "https://github.com/nvlabs/cub.git"
-    GIT_TAG "70de43938d9bed686531a5bf01bd6cbba8959884"
+    GIT_TAG "62d7334d15b27765d6d503f342c80aee9560bf8f"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
---
+++ contrib/fix-boost-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# for reasons unknown to me, some of the required boost header libraries never
+# get installed by the boost build system; this script basically does that
+# after the fact 
+
+boost_SRC=$1
+boost_INSTALL=$2
+
+for f in $boost_SRC/src/boost/libs/*/include
+do
+    cd $f
+    tar cf - . | (cd $boost_INSTALL/include && tar spxvf -)
+done
+

f310c2c427e7ad315ebde9f46c9ee567760adbc0
Nuno Subtil <nsubtil@nvidia.com>
2015-03-07 21:20:36 -0800

    build: add option to disable TSX support in Intel TBB
    Ubuntu 12 has an old version of binutils which doesn't support TSX. Add a patch
    to disable TSX instructions in TBB via a command-line build flag.

--- contrib/tbb.cmake
+++ contrib/tbb.cmake
@@ -4,11 +4,18 @@ set(tbb_INSTALL ${CMAKE_BINARY_DIR}/contrib/tbb-install)
 
 set(tbb_SRC ${tbb_PREFIX}/src/tbb)
 
+if (FORCE_TSX_OFF)
+  set(TBB_PATCH_COMMAND patch -p1 -t -N < ${PROJECT_SOURCE_DIR}/contrib/tbb-disable-tsx.patch)
+else()
+  set(TBB_PATCH_COMMAND "")
+endif()
+
 ExternalProject_Add(tbb
     PREFIX ${tbb_PREFIX}
     GIT_REPOSITORY "https://github.com/nsubtil/tbb.git"
     GIT_TAG "4.3-20140724"
     BUILD_IN_SOURCE 1
+    PATCH_COMMAND ${TBB_PATCH_COMMAND}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${PROJECT_SOURCE_DIR}/contrib/tbb-install.sh ${tbb_SRC} ${tbb_INSTALL}

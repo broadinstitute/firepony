131d3844ab397d719ad97ed366c98ae8c711f6dd
Nuno Subtil <subtil@gmail.com>
2015-06-17 18:32:27 -0700

    debian: make filter-changelog update only the latest entry
    To keep the changelog consistent, update only the latest entry in the
    changelog. This seems to be enough to generate packages for the various
    distributions.

--- debian/filter-changelog.py
+++ debian/filter-changelog.py
@@ -5,15 +5,24 @@ import debian.changelog
 
 filename = sys.argv[1]
 distribution = sys.argv[2]
-extraversion = sys.argv[3]
+
+if len(sys.argv) > 2:
+    extraversion = sys.argv[3]
+else:
+    extraversion = ""
 
 fp = open(filename)
 data = fp.read()
 
 changelog = debian.changelog.Changelog(data)
+changelog_iter = iter(changelog)
 
-for change in changelog:
-	change.version = str(change.version) + distribution + extraversion
-	change.distributions = distribution
+# modify the first change to point at our distribution, but leave all others alone
+change = changelog_iter.next()
+change.version = str(change.version) + distribution + extraversion
+change.distributions = distribution
+print change
+
+for change in changelog_iter:
 	print change
 

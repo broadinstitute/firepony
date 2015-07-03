a8c3e9ba3e30b78494f8b5e26b8d9b308873de79
Nuno Subtil <subtil@gmail.com>
2015-07-03 16:15:24 -0700

    vector: Add strided iterator
    Extremely simple and dumb iterator with a fixed stride, meant to wrap
    pointers/vectors for GPU access.

--- device/primitives/vector.h
+++ device/primitives/vector.h
@@ -1,6 +1,9 @@
 /*
  * Firepony
- * Copyright (c) 2014-2015, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Copyright (c) 2014-2015, NVIDIA CORPORATION
+ * Copyright (c) 2015, Nuno Subtil <subtil@gmail.com>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -9,20 +12,20 @@
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of the NVIDIA CORPORATION nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
+ *    * Neither the name of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #pragma once
@@ -275,5 +278,57 @@ void cross_device_copy(int dest_device, vector<sys_dest, T>& dest, size_t dest_o
 #endif
         thrust::copy_n(source.begin() + source_offset, len, dest.begin() + dest_offset);
 }
+
+
+template <typename T, uint32 stride, typename IndexType = uint64>
+struct strided_iterator
+{
+    typedef T*                                                          iterator;
+    typedef const T*                                                    const_iterator;
+    typedef typename thrust::iterator_traits<iterator>::value_type      value_type;
+    typedef typename thrust::iterator_traits<iterator>::reference       reference;
+    typedef typename thrust::iterator_traits<const_iterator>::reference const_reference;
+    typedef typename thrust::iterator_traits<iterator>::pointer         pointer;
+    typedef typename thrust::iterator_traits<const_iterator>::pointer   const_pointer;
+    typedef typename thrust::reverse_iterator<iterator>                 reverse_iterator;
+    typedef typename thrust::reverse_iterator<const_iterator>           const_reverse_iterator;
+    typedef typename thrust::iterator_traits<iterator>::difference_type difference_type;
+    typedef IndexType                                                   size_type;
+
+    CUDA_HOST_DEVICE
+    strided_iterator() = default;
+
+    CUDA_HOST_DEVICE
+    strided_iterator(T *base)
+        : m_vec(base)
+    { }
+
+    CUDA_HOST_DEVICE inline size_type offset(size_type elem) const
+    {
+        return elem * stride;
+    }
+
+    CUDA_HOST_DEVICE reference operator[](size_type n)
+    {
+        return m_vec[offset(n)];
+    }
+
+    CUDA_HOST_DEVICE const_reference operator[](size_type n) const
+    {
+        return m_vec[offset(n)];
+    }
+
+    CUDA_HOST_DEVICE reference at(size_type n)
+    {
+        return m_vec[offset(n)];
+    }
+
+    CUDA_HOST_DEVICE const_reference at(size_type n) const
+    {
+        return m_vec[offset(n)];
+    }
+
+    T *m_vec;
+};
 
 } // namespace firepony

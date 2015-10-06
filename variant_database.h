6d129b52cbbc4b3c03b6310e3c116e43d3cbcc1a
Nuno Subtil <subtil@gmail.com>
2015-10-05 21:12:29 -0700

    variant_database: Convert to Lift memory containers

--- variant_database.h
+++ variant_database.h
@@ -1,6 +1,9 @@
 /*
  * Firepony
- * Copyright (c) 2014-2015, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Copyright (c) 2014-2015, NVIDIA CORPORATION
+ * Copyright (c) 2015, Nuno Subtil <subtil@gmail.com>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -9,20 +12,20 @@
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of the NVIDIA CORPORATION nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
+ *    * Neither the name of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #pragma once
@@ -39,27 +42,27 @@ namespace firepony {
 template <target_system system>
 struct variant_storage
 {
-    vector<system, uint32> feature_start;           // feature start position in the reference
-    vector<system, uint32> feature_stop;            // feature stop position in the reference
+    persistent_allocation<system, uint32> feature_start;           // feature start position in the reference
+    persistent_allocation<system, uint32> feature_stop;            // feature stop position in the reference
 
     // contains a prefix scan of the end points using max as the operator
     // each element encodes the maximum end point of any feature to the left of it
-    vector<system, uint32> max_end_point_left;
+    persistent_allocation<system, uint32> max_end_point_left;
 
-    variant_storage<system>& operator=(const variant_storage<host>& other)
+    variant_storage& operator=(const variant_storage<host>& other)
     {
-        feature_start = other.feature_start;
-        feature_stop = other.feature_stop;
+        feature_start.copy(other.feature_start);
+        feature_stop.copy(other.feature_stop);
         // xxxnsubtil: recompute this instead of moving across PCIE?
-        max_end_point_left = other.max_end_point_left;
+        max_end_point_left.copy(other.max_end_point_left);
         return *this;
     }
 
     struct const_view
     {
-        typename vector<system, uint32>::const_view feature_start;
-        typename vector<system, uint32>::const_view feature_stop;
-        typename vector<system, uint32>::const_view max_end_point_left;
+        persistent_allocation<system, uint32> feature_start;
+        persistent_allocation<system, uint32> feature_stop;
+        persistent_allocation<system, uint32> max_end_point_left;
     };
 
     operator const_view() const
